db31e49a5c96abb4752ac4515b309c03ac079127
gzh <936285702@qq.com>
2021-05-17 19:03:32 +0800

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-17 18:11:33
+ * @LastEditTime: 2021-05-17 18:17:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3\vue-app\babel.config.js
+ */
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "vant",
+        libraryDirectory: "es",
+        style: true
+      },
+      "vant"
+    ]
   ]
-}
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-button type="primary" block>块级元素</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+
+    <van-cell title="单元格" value="内容" size="large" />
+    <van-cell title="单元格" value="内容" size="large" label="描述信息" />
+
+    <van-cell title="单元格" icon="location-o" />
+
+    <div class="test-viewport">测试转换</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import { Button, Cell, CellGroup } from "vant";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    [Button.name]: Button,
+    [Cell.name]: Cell,
+    [CellGroup.name]: CellGroup
   }
-}
+};
 </script>
+
+<style lang="less" scoped>
+.test-viewport {
+  width: 750px;
+  height: 100px;
+  font-size: 40px;
+  text-align: center;
+  line-height: 100px;
+  background: #13b5b1;
+}
+</style>
