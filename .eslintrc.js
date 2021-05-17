6c487f7ee69ff5bd72230b8f7b5c54c530445b55
gzh <936285702@qq.com>
2021-05-17 19:17:42 +0800

    配合uni-app 打包成安卓apk的一些配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-17 18:11:33
+ * @LastEditTime: 2021-05-17 18:32:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3\vue-app\.eslintrc.js
+ */
 module.exports = {
   root: true,
   env: {
@@ -12,6 +20,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': 0,
+    "quotes": 0
   }
 }
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-17 19:05:11
+ * @LastEditTime: 2021-05-17 19:05:59
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3\vue-app\vue.config.js
+ */
+module.exports = {
+  publicPath: "./"
+};
