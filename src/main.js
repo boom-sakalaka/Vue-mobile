85c5572683904f3f48a3d81112c882c137239d6f
gzh <936285702@qq.com>
2021-05-19 09:58:36 +0800

    配置多环境打包

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+/*
+ * @Author: your name
+ * @Date: 2021-05-17 18:11:33
+ * @LastEditTime: 2021-05-19 09:57:29
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-app\src\main.js
+ */
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+console.log(process.env.VUE_APP_BASE_URL);
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-05-17 18:11:33
+ * @LastEditTime: 2021-05-19 09:56:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-app\src\views\About.vue
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
